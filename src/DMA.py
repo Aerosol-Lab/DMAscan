Fixing issues with DMA center diameter calculation and flow conversion to SI units

src/DMA.py
@@ -72,10 +72,10 @@ class DMAscan:
         self.Vstep=int(entriesScan[3].get())
         self.delayTime=float(entriesScan[4].get())
         self.HVmode=int(entriesScan[5].get())
-        Rratio=float(entriesDMA[1].get())/float(entriesDMA[2].get())
+        Rratio=float(entriesDMA[2].get())/float(entriesDMA[1].get())
         L=float(entriesDMA[0].get())*1e-3
-        Q=(float(entriesDMA[3].get())+float(entriesDMA[4].get()))/6e4
-        self.coeff=Q*np.log(Rratio)/(2.0*np.pi*L)
+        Q_sheath = (float(entriesDMA[3].get())) * (1.66667e-5)
+        self.coeff = 2*Q_sheath*np.log(Rratio)/(4.0*np.pi*L)
         self.Cs, self.Vs, self.Dps, self.Zps = [], [], [], []
         self.updateFlag=1
         self.stop=0

src/main.py
@@ -67,7 +67,7 @@ def main():
     labelsScan=np.array(["Min voltage","Max voltage","Time per a bin","Number of bins","Delay time","HV mode","CPC mode"])
     labelsHV=np.array(["      Slope      ","Bias"])
     labelsFix=np.array(["      Voltage      ","CPC mode"])
-    labelsDMA=np.array(["Lenght","Inner radiusr","Outer radius","Sheath flow","Aerosol flow"])
+    labelsDMA=np.array(["Lenght","Inner radius","Outer radius","Sheath flow","Aerosol flow"])
 
     # Units of variables
     unitsDAQ=np.array([" ","V","V"," ","V","V"])
@@ -81,7 +81,7 @@ def main():
     initialsScan=np.array([10,1000,5,10,2,0,-1])
     initialsHV=np.array([3999.1,-0.9668])
     initialsFix=np.array([10,-1])
-    initialsDMA=np.array([443,19.61,9.37,3,0.3])
+    initialsDMA=np.array([443,9.37,19.61,3,0.3])
 
     # Initialize entries
     entriesDAQ=[]